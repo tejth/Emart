10f66eb94e31544a5d83d46ae5995a1450fc6ccb
tejth <ptejendra91@gmail.com>
2024-11-18 20:38:11 +0530

    inital setup

---
+++ src/customer/components/HomeCarosel/MainCaroselData.jsx
@@ -0,0 +1,18 @@
+export const MainCaroselData = [
+  {
+    image: "",
+    path: "",
+  },
+  {
+    image: "",
+    path: "",
+  },
+  {
+    image: "",
+    path: "",
+  },
+  {
+    image: "",
+    path: "",
+  },
+];
---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MainCarosel from "../../components/HomeCarosel/MainCarosel";
+
+const HomePage = () => {
+  return (
+    <div>
+      <MainCarosel />
+    </div>
+  );
+};
+
+export default HomePage;
